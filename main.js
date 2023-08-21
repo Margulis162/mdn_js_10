64756327deb20573512e5385a80772cf58af9631
Margulis162 <markmargulis@proton.me>
2023-08-21 14:56:37 -0400

    update method

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function randomRGB() {
 }
 
 class Ball{
-  constuctor(x,y, velX, velY, color, size){
+  constructor(x,y, velX, velY, color, size){
     this.x = x;
     this.y = y;
     this.velX = velX;
@@ -28,11 +28,33 @@ class Ball{
     this.color = color;
     this.size = size;
   }
+  draw(){
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+    ctx.fill();
+  }
+  update() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+  
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+  
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+  
 }
 
-draw(){
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
-  ctx.fill();
-}
+
