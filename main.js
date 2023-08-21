84ee514ef179b48f565794c6eee373df92cdfc68
Margulis162 <markmargulis@proton.me>
2023-08-21 15:14:14 -0400

    animation function added

--- main.js
+++ main.js
@@ -72,3 +72,16 @@ while (balls.length < 25) {
   
 }
 
+function loop() {
+  ctx.fillStyle = "rgba(0,0,0, 0.25)";
+  ctx.fillRect(0,0, width, heigth);
+
+  for (const ball of balls){
+    ball.draw();
+    ball.update();
+  }
+  requestAnimationFrame(loop);
+}
+
+loop();
+
