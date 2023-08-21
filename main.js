2d5b64f4a6244d31c88fbb8b2362180da90ff4f9
Margulis162 <markmargulis@proton.me>
2023-08-21 17:22:32 -0400

    collision detection

--- main.js
+++ main.js
@@ -54,6 +54,21 @@ class Ball{
     this.x += this.velX;
     this.y += this.velY;
   }
+
+  collisionDetect(){
+    for (const ball of balls){
+      // makes sure we are checking for a different ball 
+      if(this !== ball) {
+        // standart collision detection method for two circles 
+        const dx = this.x -ball.x;
+        const dy = this.y -ball.y;
+        const distance = Math.sqrt(dx*dx + dy*dy);
+        if(distance < this.size + ball.size){
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
   
 }
 
@@ -74,13 +89,14 @@ while (balls.length < 25) {
 }
 
 function loop() {
-  ctx.fillStyle = "rgba(0,0,0, 0.25)";
+  ctx.fillStyle = "rgba(0,0,0, .1)";
   ctx.fillRect(0,0, width, height);
 
   for (const ball of balls){
     ball.draw();
     ball.update();
-  }
+    ball.collisionDetect();
+    }
   requestAnimationFrame(loop);
 }
 
